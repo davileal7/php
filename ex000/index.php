4bda70aa8b85a25dd24bd8be9d1d930c94443726
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-01-27 12:47:37 -0300

    primeiras aulas

---
+++ ex000/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1° código php</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Olá mundo! \u{1F30D}"
+        ?>
+        <?php 
+        print "Olá PHP \u{1F418}"
+        ?>
+    </h1>
+    <p>
+        <?php echo "Por que Elefante é o mascote de PHP?"  ?>
+    </p>
+    <p>
+        <?php echo "O logo do php é uma mascote e chama-se 'ElePHPant'. Foi criado por Vincent Pontier, em um determinado ângulo da letra ele via um elefante."?>
+    </p>
+   
+      
+</body>
+</html>
